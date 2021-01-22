0e3282cfb686384ce3127abc89867ae4b5be7951
Barry <Barry-B15@users.noreply.github.com>
2021-01-23 01:14:22 +0900

    feat: props, defaultProps and types

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
     return ( 
         <header >
-            <h1>Task Tracker</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
+// set the default props
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
 
+// set prop types, notice Header. lowercase p in propTypes
+Header.propTypes = {
+    title: PropTypes.string,
+}
 export default Header
