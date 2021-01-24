d3c379c60d487b4c0ef3abfcf6cbce44ae6bff48
Barry <Barry-B15@users.noreply.github.com>
2021-01-24 15:51:02 +0900

    feat: add and set state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-//import React from "react";
+
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 
 // function  App() {
   const App = () => { 
+    // declare a new func to set state
+    const [tasks, setTasks] = useState(
+      [{
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: 'true',
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 3:30pm',
+        reminder: 'true',
+      },
+      {
+        id: 3,
+        text: 'Shopping for Food',
+        day: 'Feb 7th at 5:30pm',
+        reminder: 'false',
+      }
+      ]
+    )
   return (
      <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div> 
   );
 }
