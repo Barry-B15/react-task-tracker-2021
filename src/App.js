fa9744eb4cb6edc9d160cd3df51abbdc708691f4
Barry <Barry-B15@users.noreply.github.com>
2021-01-31 17:33:43 +0900

    feat: create AddTaskJS component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,68 @@
-
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
 
 
 // function  App() {
-  const App = () => { 
+const App = () => {
     // declare a new func to set state
     const [tasks, setTasks] = useState(
-      [{
-        id: 1,
-        text: 'Doctor Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: 'true',
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 3:30pm',
-        reminder: 'true',
-      },
-      {
-        id: 3,
-        text: 'Shopping for Food',
-        day: 'Feb 7th at 5:30pm',
-        reminder: 'false',
-      }
-      ]
+        [{
+                id: 1,
+                text: 'Doctor Appointment',
+                day: 'Feb 5th at 2:30pm',
+                reminder: 'true',
+            },
+            {
+                id: 2,
+                text: 'Meeting at School',
+                day: 'Feb 6th at 3:30pm',
+                reminder: 'true',
+            },
+            {
+                id: 3,
+                text: 'Shopping for Food',
+                day: 'Feb 7th at 5:30pm',
+                reminder: 'false',
+            }
+        ]
     )
 
     //Delete Task
     const deleteTask = (id) => {
-      setTasks(tasks.filter((task) => task.id !== id))
+        setTasks(tasks.filter((task) => task.id !== id))
     }
 
     // create toggle reminder
     const toggleReminder = (id) => {
-      // console.log(id)
-      setTasks(
-        tasks.map((task) => 
-        task.id === id ? {...task, reminder : 
-          !task.reminder} : task)
-      )
+        // console.log(id)
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? {...task,
+                    reminder:
+                        !task.reminder
+                } : task)
+        )
     }
 
-  return (
-     <div className="container">
-      <Header />
-    {tasks.length > 0 ?  (   // if the length of tasks is greater than 0
-      <Tasks tasks={tasks}  
-      onDelete={deleteTask} //pass the func to onDelete
-      onToggle={toggleReminder} // pass the toggle
-       />
-       ) : ( // else
-         'No Tasks To Show' // show a msg
-         ) } 
-    </div> 
-  );
+    return ( 
+      <div className = "container" >
+        <Header / > 
+        <AddTask />
+        {
+            tasks.length > 0 ? ( // if the length of tasks is greater than 0
+                <Tasks tasks = { tasks }
+                onDelete = { deleteTask } //pass the func to onDelete
+                onToggle = { toggleReminder } // pass the toggle
+                />
+            ) : ( // else
+                'No Tasks To Show' // show a msg
+            )
+        } 
+        </div> 
+    );
 }
 
 // class App extends React.Component {
